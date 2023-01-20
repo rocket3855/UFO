3ad3981057f41abd57114a9d8e4e914b47c2bfa3
rocket3855 <69988487+rocket3855@users.noreply.github.com>
2023-01-20 05:21:31 -0500

    Module 11

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -24,30 +24,30 @@ function buildTable(data) {
   });
 }
 
-// 1. Create a variable to keep track of all the filters as an object.
+//Create a variable to keep track of all the filters as an object.
 var filters = {};
 
-// 3. Use this function to update the filters. 
-function forClick() {
+// Use this function to update the filters. 
+function updateFilters() {
 
-  // 4a. Save the element that was changed as a variable.
-  // 4b. Save the value that was changed as a variable.
-  // 4c. Save the id of the filter that was changed as a variable.
+  // Save the element that was changed as a variable.
+  // Save the value that was changed as a variable.
+  // Save the id of the filter that was changed as a variable.
   let dates = d3.select("#datetime").property("value");
   let Data = tableData;
 
-  // 5. If a filter value was entered then add that filterId and value
+  // If a filter value was entered then add that filterId and value
   // to the filters list. Otherwise, clear that filter from the filters object.
   if (dates) {
     Data = Data.filter(row => row.datetime === dates);
   }
 
- // 6. Call function to apply all filters and rebuild the table
+ // Call function to apply all filters and rebuild the table
   buildTable(Data);
 }
 
-// 2. Attach an event to listen for changes to each filter
-d3.selectAll("#filter-btn").on("click", forClick);
+// Attach an event to listen for changes to each filter
+d3.selectAll("#filter-btn").on("click", updateFilters);
 
 // Build the table when the page loads
 buildTable(tableData);
